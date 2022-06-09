5600411ff6fc88b2f6063e8da82f9dbb0d377e97
Johan Linol <linol.johan@gmail.com>
2022-06-09 16:30:50 +0200

    first commit. Initialized the project. created most of the files needed

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return <MainContainer></MainContainer>;
+}
+
+export default App;
+
+const MainContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+`;
---
+++ src/Components/Infos/infos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Infos(props) {
+  return <div></div>;
+}
+
+export default Infos;
