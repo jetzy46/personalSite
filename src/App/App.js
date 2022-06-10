6eb716444ca0881547ce636f35f6afcbb8fdfa1c
Johan Linol <linol.johan@gmail.com>
2022-06-10 19:34:48 +0200

    done the header"
    "

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../Components/Header/header";
 
 function App() {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <Header />
+    </MainContainer>
+  );
 }
 
 export default App;
---
+++ src/Components/FilesSection/files.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Files(props) {
+  return <div></div>;
+}
+
+export default Files;
